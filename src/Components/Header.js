015639be2018517b26cf000aff093bad11ac1573
ahmed <sida7medess@gmail.com>
2017-12-08 00:38:30 +0100

    added html and css for my weatherinfo component

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -9,27 +9,12 @@ function Gallery (props){
     <div id="nav">
   		<div className="orange">
   		</div>
-  		<h5 className="title"> Profile Page</h5>
+  		<h5 className="title"> Weather and News </h5>
   		<input type="text" name="" value={props.searchValue} onChange={props.onChangeSearch} className="recherche" placeholder="search here !"/>
   	</div>
-		<div id="left">
-
-		</div>
-		<div id="right">
-			<div class="cercle"> 1</div>
-			<div class="cercle"> 2</div>
-			<div class="cercle"> 3</div>
-			<div class="cercle"> 4</div>
-			<div class="cercle"> 4</div>
-			<div class="purple"></div>
-	  </div>
 	</div>
 
 /////////////////////////////////////////////////////:::
-      // <div classNameName="header">
-      // <label>Search</label>
-      // <input type="text" name="" value={props.searchValue} onChange={props.onChangeSearch}/>
-      // </div>
 		);
 
 }
--- src/Components/WeatherInfo.js
+++ src/Components/WeatherInfo.js
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import photo1 from "./feel.png";
+import photo2 from "./rain.png";
+import photo3 from "./wind.png";
+import photo4 from "./weather-bottom.png";
 //====================================
 class WeatherInfo extends React.Component
 {
@@ -13,7 +16,41 @@ class WeatherInfo extends React.Component
   render() {
     return (
       <div className="weatherInfo">
-        {this.props.weather[0].formatted_address}
+        <div class="container">
+          <div class="up">
+            <div class="left">
+              <div class="date">Saturday, March 26th</div>
+              <div class="place">San Francisco, CA</div>
+            </div>
+            <div class="center">
+              <div class="temp">64°F</div>
+              <span>low:55 </span><span> hight:55</span>
+              <div class="climate">Partly Sunny</div>
+            </div>
+            <div class="right">
+              <div>updated : 07/12/2017</div>
+              </div>
+          </div>
+          <div class="down">
+            <div class="desc">
+              <div class="img"><img src={photo1}></img></div>
+              <div>Real feel</div>
+              <div>67°</div>
+            </div>
+            <div class="desc">
+            <div class="img"><img src={photo2}></img></div>
+            <div>Chances of rain</div>
+            <div>5%</div>
+            </div>
+            <div class="desc">
+            <div class="img"><img src={photo3}></img></div>
+            <div>Wind speed</div>
+            <div>20MPH</div>
+          </div>
+          <div class="weather-bottom"><img src={photo4} ></img></div>
+        </div>
+
+        </div>
     	</div>
 	    );
 	  }
